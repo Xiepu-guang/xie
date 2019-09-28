def882848c50473ae9034c264f061705abc38f17
Administrator <2559199600@qq.com>
2019-09-28 19:53:29 +0800

    qqqqqqqqqqqq

--- RunMainWinHorizontalLayout.py
+++ RunMainWinHorizontalLayout.py
@@ -1,11 +1,11 @@
 import sys
-import MainWinHorizontalLayout
+import aaa
 from PyQt5.QtWidgets import QApplication,QMainWindow
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     mainWindow = QMainWindow()
-    ui = MainWinHorizontalLayout.Ui_MainWindow()
+    ui = aaa.Ui_MainWindow()
     ui.setupUi(mainWindow)
     mainWindow.show()
     sys.exit(app.exec_())
